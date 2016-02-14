4aff3c4dd2626904b88855f5c3ff97228ee0a112
tikkal <tikkal@users.noreply.github.com>
2016-02-14 13:25:44 -0500

    support multiple options.

--- runner.py
+++ runner.py
@@ -13,8 +13,13 @@ def call_with_cwd(project, exec_args):
   p = subprocess.Popen(exec_args, cwd=project)
   p.wait()
   
-def cargo_build_or_test(project, build_or_test, use_mock_routing=False):
-  cargo_args = ["cargo", build_or_test]
+def cargo_build_or_test(project, options, use_mock_routing=False):
+  if "test" in options:
+    print "test " + project
+    cargo_args = ["cargo", "test"]
+  else:
+    print "build " + project
+    cargo_args = ["cargo", "build"]
 
   # Add mock routing feature if specified
   if (use_mock_routing): 
@@ -22,7 +27,7 @@ def cargo_build_or_test(project, build_or_test, use_mock_routing=False):
 
   call_with_cwd(project, cargo_args)
 
-def do_runner(option):
+def do_runner(options):
   projects = [
     "accumulator",
     "crust",
@@ -48,23 +53,26 @@ def do_runner(option):
     "safe_launcher"
   ]
   for project in projects:
-    print option + " " + project
-    if option == "build" or option == "test":
-      mock_routing = project in mock_routing_projects
-      cargo_build_or_test(project, option, use_mock_routing=mock_routing)
-    elif option == "clean":
-      call_with_cwd(project, ["cargo", "clean"])
-    elif option == "clone":
+    if "clone" in options:
+      print "git clone " + project
       url = "https://github.com/maidsafe/" + project + ".git"
       subprocess.call(["git", "clone", url])
-    elif option == "pull":
+    if "pull" in options:
+      print "git pull " + project
       call_with_cwd(project, ["git", "pull"])
-    elif option == "update":
+    if "clean" in options:
+      print "cargo clean " + project
+      call_with_cwd(project, ["cargo", "clean"])
+    if "update" in options:
+      print "cargo update " + project
       call_with_cwd(project, ["cargo", "update"])
+    if "build" in options or "test" in options:
+      mock_routing = project in mock_routing_projects
+      cargo_build_or_test(project, options, use_mock_routing=mock_routing)
 
 # Check for a few simple command line args.
-if len(sys.argv) != 2:
-  print "Please specify one of: build, clean, clone, pull, test, update"
+if len(sys.argv) < 2:
+  print "Please specify at least one of: build, clean, clone, pull, test, update"
 else: 
-  do_runner(sys.argv[1])
+  do_runner(sys.argv)
 
