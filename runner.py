365ba2e8963b59fc0c2f11598e91e1f405d48e8c
tikkal <tikkal@users.noreply.github.com>
2016-02-13 22:37:43 -0500

    support cleaning option

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import subprocess
+import sys
 
 def cargo_exec(project, cargo_args):
   p = subprocess.Popen(cargo_args, cwd=project)
@@ -13,38 +14,43 @@ def cargo_test(project, use_mock_routing=False):
   if (use_mock_routing): 
     cargo_args.extend(["--features", "use-mock-routing"])
 
-  cargo_exec(cargo_args, project)
+  cargo_exec(project, cargo_args)
 
-def do_runner():
+def do_runner(cleaning=False):
   # Projects that require mock routing
   projects = [
     "accumulator",
+    "crust",
     "drive",
     "kademlia_routing_table", 
     "lru_time_cache",
     "maidsafe_utilities",
     "message_filter",
-    "routing crust",
-    "rust-utp", 
-    "QA", 
-    "rfcs", 
+    "routing",
+    "rust-utp",
     "safe_vault",
     "safe_ffi",
     "safe_launcher",
     "self_encryption",
     "safe_dns",
     "safe_launcher",
-    "safe_nfs safe_core", 
+    "safe_nfs",
+    "safe_core", 
     "xor_name"
   ]
   mock_routing_projects = [
     "safe_dns",
     "safe_launcher"
   ]
-  for (project in projects):
-    mock_routing = project in mock_routing_projects
-    cargo_test(project, use_mock_routing=mock_routing)
+  for project in projects:
+    if cleaning:
+      print "cleaning " + project
+      cargo_exec(project, ["cargo", "clean"])
+    else:
+      print "building and testing " + project
+      mock_routing = project in mock_routing_projects
+      cargo_test(project, use_mock_routing=mock_routing)
 
-do_runner()
-
+cleaning = len(sys.argv) >= 2 and sys.argv[1] == "clean"
+do_runner(cleaning)
 
