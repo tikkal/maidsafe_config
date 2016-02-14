e724b98b906942a9aef4ba5a941251a8f0748162
tikkal <tikkal@users.noreply.github.com>
2016-02-13 22:47:43 -0500

    add clone option

--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 
 def cargo_exec(project, cargo_args):
+  # Use Popen to set directory in project
   p = subprocess.Popen(cargo_args, cwd=project)
   p.wait()
   
@@ -16,7 +17,7 @@ def cargo_test(project, use_mock_routing=False):
 
   cargo_exec(project, cargo_args)
 
-def do_runner(cleaning=False):
+def do_runner(cleaning=False, cloning=False):
   # Projects that require mock routing
   projects = [
     "accumulator",
@@ -46,11 +47,17 @@ def do_runner(cleaning=False):
     if cleaning:
       print "cleaning " + project
       cargo_exec(project, ["cargo", "clean"])
+    elif cloning:
+      print "cloning " + project
+      url = "https://github.com/maidsafe/" + project + ".git"
+      subprocess.call(["git", "clone", url])
     else:
       print "building and testing " + project
       mock_routing = project in mock_routing_projects
       cargo_test(project, use_mock_routing=mock_routing)
 
+# Check for a few simple command line args.
 cleaning = len(sys.argv) >= 2 and sys.argv[1] == "clean"
-do_runner(cleaning)
+cloning = len(sys.argv) >= 2 and sys.argv[1] == "clone"
+do_runner(cleaning, cloning)
 
