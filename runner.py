975b368a0ee3b4309e28f2b3aeae04a6b966b8d8
tikkal <tikkal@users.noreply.github.com>
2016-02-15 17:25:51 -0500

    Add environment checks.

--- runner.py
+++ runner.py
@@ -3,8 +3,10 @@
 # Note, in your .bashrc, put: 
 #  export SODIUM_LIB_DIR=/usr/local/lib
 #  export RUST_TEST_THREADS=1
-# See maidsafe forums for info re. installing libsodium.
+# See maidsafe forums for info re. installing libsodium
+# and other configuration tips.
 
+import os
 import subprocess
 import sys
 
@@ -69,10 +71,36 @@ def do_runner(options):
     if "build" in options or "test" in options:
       mock_routing = project in mock_routing_projects
       cargo_build_or_test(project, options, use_mock_routing=mock_routing)
+  
+def run_env_checks():
+  print "Running environment checks."
+  ret = True
+
+  # Check required executables 
+  for prog in ["cargo", "rustc", "git"]:
+    try:
+      subprocess.call([prog, "--version"])
+    except:
+      print "** Trouble finding version of " + prog
+      ret = False
+
+  # Check required env vars
+  for env_key in ["RUST_TEST_THREADS", "SODIUM_LIB_DIR"]:
+    env_val = os.environ.get(env_key)
+    if env_val is None:
+      print "** " + env_key + " env var not set."
+      ret = False
+    else:
+      print env_key + " is " + env_val
+  return ret
 
 # Check for a few simple command line args.
+options = ["build", "clean", "clone", "pull", "test", "update"]
 if len(sys.argv) < 2:
-  print "Please specify at least one of: build, clean, clone, pull, test, update"
+  print "Please specify at least one of: " + ", ".join(options)
+elif (not run_env_checks()):
+  print "Environment checks failed." 
+  print "See maidsafe forums for tips on fixing your environment."
 else: 
   do_runner(sys.argv)
 
