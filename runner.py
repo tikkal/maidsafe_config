3828f10c2ac85d7e0343596ca80e8486b8f0730b
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-09-19 11:13:53 +0100

    Removed obsolete requirement for setting some environment variables.

--- runner.py
+++ runner.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-# Note, in your .bashrc, put: 
-#  export SODIUM_LIB_DIR=/usr/local/lib
-#  export RUST_TEST_THREADS=1
-# See maidsafe forums for info re. installing libsodium
-# and other configuration tips.
-
 import os
 import subprocess
 import sys
@@ -14,7 +8,7 @@ def call_with_cwd(project, exec_args):
   # Use Popen to set directory in project
   p = subprocess.Popen(exec_args, cwd=project)
   p.wait()
-  
+
 def cargo_build_or_test(project, options, use_mock_routing=False):
   if "test" in options:
     print "test " + project
@@ -24,7 +18,7 @@ def cargo_build_or_test(project, options, use_mock_routing=False):
     cargo_args = ["cargo", "build"]
 
   # Add mock routing feature if specified
-  if (use_mock_routing): 
+  if (use_mock_routing):
     cargo_args.extend(["--features", "use-mock-routing"])
 
   call_with_cwd(project, cargo_args)
@@ -34,26 +28,25 @@ def do_runner(options):
     "accumulator",
     "config_file_handler",
     "crust",
-    "drive",
-    "kademlia_routing_table", 
+    "kademlia_routing_table",
     "lru_time_cache",
     "maidsafe_utilities",
     "routing",
     "rust_sodium",
-    "safe_core", 
+    "safe_core",
     "safe_launcher",
     "safe_vault",
     "secure_serialisation",
     "self_encryption",
   ]
   mock_routing_projects = [
-    "safe_core", 
+    "safe_core",
     "safe_launcher",
   ]
   node_projects = [
     "safe_launcher"
   ]
- 
+
   for project in projects:
     if "clone" in options:
       print "git clone " + project
@@ -72,27 +65,18 @@ def do_runner(options):
       if "build" in options or "test" in options:
         mock_routing = project in mock_routing_projects
         cargo_build_or_test(project, options, use_mock_routing=mock_routing)
-  
+
 def check_env():
   print "Running environment checks."
   ret = True
 
-  # Check required executables 
+  # Check required executables
   for prog in ["cargo", "rustc", "git"]:
     try:
       subprocess.call([prog, "--version"])
     except:
       print "** Trouble finding version of " + prog
       ret = False
-
-  # Check required env vars
-  for env_key in ["RUST_TEST_THREADS", "SODIUM_LIB_DIR"]:
-    env_val = os.environ.get(env_key)
-    if env_val is None:
-      print "** " + env_key + " env var not set."
-      ret = False
-    else:
-      print env_key + " is " + env_val
   return ret
 
 # Check for a few simple command line args.
@@ -100,7 +84,7 @@ options = ["build", "clean", "clone", "pull", "test", "update"]
 if len(sys.argv) < 2:
   print "Please specify at least one of: " + ", ".join(options)
 elif not check_env():
-  print "Environment checks failed." 
-  print "See maidsafe forums for tips on fixing your environment."
-else: 
+  print "Environment checks failed."
+  print "See MaidSafe forums for tips on fixing your environment."
+else:
   do_runner(sys.argv)
